cc1de6e55bd1eddf98cb7329f4f2ee00be7c5cc9
izabera <izaberina@gmail.com>
2016-08-21 23:47:23 +0200

    disable klibc

--- qsort.c
+++ qsort.c
@@ -133,7 +133,7 @@ int main(int argc, char *argv[]) {
     {  uclibc_qsort, "uclibc" , 0, 0 },
     {   plan9_qsort, "plan9"  , 0, 0 },
     { illumos_qsort, "illumos", 0, 0 },
-    {   klibc_qsort, "klibc"  , 0, 0 },
+    /*{   klibc_qsort, "klibc"  , 0, 0 },*/  // quadratic time on 50% sorted input
     {      my_qsort, "mine"   , 0, 0 },
   };
 
