e6336492124d34a2341f8a6198943e0fa2b241c4
izabera <izaberina@gmail.com>
2016-08-29 03:49:00 +0200

    reactos

--- qsort.c
+++ qsort.c
@@ -23,6 +23,7 @@ void    wada_qsort(void *base, size_t nmemb, size_t size, int (*compar)(const vo
 void freebsd_qsort(void *base, size_t nmemb, size_t size, int (*compar)(const void *, const void *));
 void   linux_qsort(void *base, size_t nmemb, size_t size, int (*compar)(const void *, const void *));
 void      ms_qsort(void *base, size_t nmemb, size_t size, int (*compar)(const void *, const void *));
+void reactos_qsort(void *base, size_t nmemb, size_t size, int (*compar)(const void *, const void *));
 
 
 
@@ -178,6 +179,7 @@ int main(int argc, char *argv[]) {
     {    wada_qsort, "wada"   , 0, 0 },
     { freebsd_qsort, "freebsd", 0, 0 },
     {   linux_qsort, "linux"  , 0, 0 },
+    /*{ reactos_qsort, "reactos", 0, 0 },*/  // quadratic time on dutch flag
     /*{      ms_qsort, "ms"     , 0, 0 },*/  // quadratic time on even odd and dutch flag
     /*{  sortix_qsort, "sortix" , 0, 0 },*/  // quadratic time on triangle and all equal
     /*{   klibc_qsort, "klibc"  , 0, 0 },*/  // quadratic time on 50% sorted input
